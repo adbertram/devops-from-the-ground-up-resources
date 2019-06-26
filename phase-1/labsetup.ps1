ee5d58bae29b71cebd3a1fdc0b8c4b069d401116
Adam Bertram <adbertram@gmail.com>
2019-06-26 16:16:29 -0500

    update

--- phase-1/labsetup.ps1
+++ phase-1/labsetup.ps1
@@ -9,13 +9,16 @@ $templatePath = "$env:TEMP\phase1lab.json"
 $url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/phase-1/phase1lab.json'
 Invoke-WebRequest -Uri $url -OutFile $templatePath
 
+$rgName = 'DevOpsFromTheGroundUpCourse-Phase1'
+
 ## Create the phase's resource group
-New-AzResourceGroup -Name DevOpsFromTheGroundUpCourse-Phase1 -Location $AzureRegion
+if (-not (Get-AzResourceGroup -Name $rgName -Location $AzureRegion -ErrorAction Ignore)) {
+	New-AzResourceGroup -Name $rgName -Location $AzureRegion
+}
 
 ## Deploy phase 1 lab
 $deploymentName = 'DFTGU-Phase1'
-$rgName = 'DevOpsFromTheGroundUpCourse-Phase1'
 $null = New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templatePath -Verbose
 
-Write-Host 'CreatedYour phase 1 lab VM IPs to RDP to are:'
+Write-Host 'Your phase 1 lab VM IPs to RDP to are:'
 (Get-AzResourceGroupDeployment -ResourceGroupName $rgName -Name $deploymentName).Outputs.resourceID.value
