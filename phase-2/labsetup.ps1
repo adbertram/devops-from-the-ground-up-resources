d06c44c335b5f73941e99cc03360de52fed90d27
Adam Bertram <adbertram@gmail.com>
2019-06-26 16:18:54 -0500

    updates

---
+++ phase-2/labsetup.ps1
@@ -0,0 +1,24 @@
+param(
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$AzureRegion = 'East US'
+)
+
+## Download the ARM template
+$templatePath = "$env:TEMP\phase2lab.json"
+$url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/phase-1/phase2lab.json'
+Invoke-WebRequest -Uri $url -OutFile $templatePath
+
+$rgName = 'DevOpsFromTheGroundUpCourse-Phase1'
+
+## Create the phase's resource group
+if (-not (Get-AzResourceGroup -Name $rgName -Location $AzureRegion -ErrorAction Ignore)) {
+	$null = New-AzResourceGroup -Name $rgName -Location $AzureRegion
+}
+
+## Deploy phase 1 lab
+$deploymentName = 'DFTGU-Phase2'
+$null = New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templatePath -Verbose
+
+Write-Host 'Your phase 1 lab VM IPs to RDP to are:'
+(Get-AzResourceGroupDeployment -ResourceGroupName $rgName -Name $deploymentName).Outputs.resourceID.value
