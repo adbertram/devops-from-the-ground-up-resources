63001216340509f9f9ab482f42b6710b9532681e
Adam Bertram <adbertram@gmail.com>
2019-06-27 09:39:51 -0500

    lab updates

--- phase-1/labsetup.ps1
+++ phase-1/labsetup.ps1
@@ -4,11 +4,14 @@ param(
 	[string]$AzureRegion = 'East US'
 )
 
+if (-not (Get-AzContext)) {
+	Write-Host "You are not authenticated to Azure. Please use the Connect-AzAccount command to provide credentials." -ForegroundColor Red
+} else {
+	## Download the main lab setup script
+	$labSetupPath = "$env:TEMP\phase1-labsetup.ps1"
+	$url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/main-labsetup.ps1'
+	Invoke-WebRequest -Uri $url -OutFile $labSetupPath
 
-## Download the main lab setup script
-$labSetupPath = "$env:TEMP\phase1-labsetup.ps1"
-$url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/main-labsetup.ps1'
-Invoke-WebRequest -Uri $url -OutFile $labSetupPath
-
-## Execute the lab setup script for the phase
-& $labSetupPath -AzureRegion $AzureRegion -CoursePhase 1 -RequiredRdpVM 'WIN10'
+	## Execute the lab setup script for the phase
+	& $labSetupPath -AzureRegion $AzureRegion -CoursePhase 1 -RequiredRdpVM 'WIN10'
+}
