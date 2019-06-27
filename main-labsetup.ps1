99613fd0cd1ea453b28a752791f916791db140a3
Adam Bertram <adbertram@gmail.com>
2019-06-27 09:09:01 -0500

    updates

---
+++ main-labsetup.ps1
@@ -0,0 +1,55 @@
+param(
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$AzureRegion = 'East US',
+
+	[Parameter(Mandatory)]
+	[ValidateNotNullOrEmpty()]
+	[int]$CoursePhase,
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[string]$RequiredRdpVM
+)
+
+## Download the ARM template
+$templatePath = "$env:TEMP\phase$($CoursePhase)lab.json"
+$url = "https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/phase-$($CoursePhase)/phase1lab.json"
+Invoke-WebRequest -Uri $url -OutFile $templatePath
+
+$rgName = "DevOpsFromTheGroundUpCourse-Phase$($CoursePhase)"
+
+## Create the phase's resource group
+if (-not (Get-AzResourceGroup -Name $rgName -Location $AzureRegion -ErrorAction Ignore)) {
+	$null = New-AzResourceGroup -Name $rgName -Location $AzureRegion
+}
+
+## Deploy phase 1 lab
+$deploymentName = "DFTGU-Phase$($CoursePhase)"
+$null = New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templatePath -Verbose
+
+$deploymentResult = (Get-AzResourceGroupDeployment -ResourceGroupName $rgName -Name $deploymentName).Outputs
+
+Write-Host "Your phase $($CoursePhase) lab VM IPs to RDP to are:"
+$vmIps = @()
+foreach ($val in $deploymentResult.Values.Value) {
+	$pubIp = Get-AzResource -ResourceId $val
+	$vmName = $pubIp.Id.split('/')[-1].Replace('-pubip', '')
+	$ip = (Get-AzPublicIpAddress -Name $pubip.Name).IpAddress
+	$vmIps += [pscustomobject]@{
+		Name = $vmName
+		IP   = $ip
+	}
+	Write-Host "VM: $vmName IP: $ip"
+}
+
+if ($env:OS -eq 'Windows_NT') {
+	$rdpNow = Read-Host -Prompt "RDP to the required host ($RequiredRdpVM) now (Y,N)?"
+	if ($rdpNow -eq 'Y') {
+		$requiredVM = $vmIps.where({ $_.Name -eq $RequiredRdpVM })
+		$ip = $requiredVM.IP
+		mstsc /v:$ip
+	}
+} else {
+	Write-Host "Please RDP to the VM [$($RequiredRdpVM) : $ip] now to begin course."
+}
--- phase-1/labsetup.ps1
+++ phase-1/labsetup.ps1
@@ -4,42 +4,11 @@ param(
 	[string]$AzureRegion = 'East US'
 )
 
-$requiredRdpVM = 'WIN10'
 
-## Download the ARM template
-$templatePath = "$env:TEMP\phase1lab.json"
-$url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/phase-1/phase1lab.json'
-Invoke-WebRequest -Uri $url -OutFile $templatePath
+## Download the main lab setup script
+$labSetupPath = "$env:TEMP\phase1-labsetup.ps1"
+$url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/labsetup.ps1'
+Invoke-WebRequest -Uri $url -OutFile $labSetupPath
 
-$rgName = 'DevOpsFromTheGroundUpCourse-Phase1'
-
-## Create the phase's resource group
-if (-not (Get-AzResourceGroup -Name $rgName -Location $AzureRegion -ErrorAction Ignore)) {
-	$null = New-AzResourceGroup -Name $rgName -Location $AzureRegion
-}
-
-## Deploy phase 1 lab
-$deploymentName = 'DFTGU-Phase1'
-$null = New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templatePath -Verbose
-
-Write-Host 'Your phase 1 lab VM IPs to RDP to are:'
-$deploymentResult = (Get-AzResourceGroupDeployment -ResourceGroupName $rgName -Name $deploymentName).Outputs
-
-$vmIps = @()
-foreach ($val in $deploymentResult.Values.Value) {
-	$pubIp = Get-AzResource -ResourceId $val
-	$vmName = $pubIp.Id.split('/')[-1].Replace('-pubip', '')
-	$ip = (Get-AzPublicIpAddress -Name $pubip.Name).IpAddress
-	$vmIps += [pscustomobject]@{
-		Name = $vmName
-		IP   = $ip
-	}
-	Write-Host "VM: $vmName IP: $ip"
-}
-
-$rdpNow = Read-Host -Prompt "RDP to the required host ($requiredRdpVM) now (Y,N)?"
-if ($rdpNow -eq 'Y') {
-	$requiredVM = $vmIps.where({ $_.Name -eq $requiredRdpVM })
-	$ip = $requiredVM.IP
-	mstsc /v:$ip
-}
+## Execute the lab setup script for the phase
+& $labSetupPath -AzureRegion $AzureRegion -CoursePhase 1 -RequiredRdpVM 'WIN10'
