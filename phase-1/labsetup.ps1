30dd69b0b81f5ab9b6a82c0141f1b1ed1982f9a4
Adam Bertram <adbertram@gmail.com>
2019-06-27 09:34:57 -0500

    lab setup

--- phase-1/labsetup.ps1
+++ phase-1/labsetup.ps1
@@ -7,7 +7,7 @@ param(
 
 ## Download the main lab setup script
 $labSetupPath = "$env:TEMP\phase1-labsetup.ps1"
-$url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/labsetup.ps1'
+$url = 'https://raw.githubusercontent.com/adbertram/devops-from-the-ground-up-resources/master/main-labsetup.ps1'
 Invoke-WebRequest -Uri $url -OutFile $labSetupPath
 
 ## Execute the lab setup script for the phase
